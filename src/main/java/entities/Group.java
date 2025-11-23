71a47a4eff28255e5b67b5d3a51fc975b915992e
Katie <tutu8836@gmail.com>
2025-11-23 17:19:48 -0500

    Updated entities to reflect API parameters

--- src/main/java/entities/Group.java
+++ src/main/java/entities/Group.java
@@ -3,38 +3,54 @@ package entities;
 import java.util.*;
 
 public class Group {
-    // Core properties
-    private String groupId;
-    private String groupName;
-
-    // Members and relationships
-    private List<User> members;
-    private List<Expense> expenses;
-    private Map<User, Double> balances; // Track who owes whom
+    // Core properties - parameters we need to send to the API
+    private final String groupName;
+    private final String groupType;
+    private final User groupCreator; // the user who created the group, we will need to access their properties to create a group
 
 
-    public Group(String groupId) {
-        this.groupId = groupId;
+    //properties the API will generate
+    private int groupId;
+    private final List<User> members;
+    private final List<Expense> originalDebts;
+    private final List<Expense> simplifiedDebts;
+
+    //private Map<User, Double> balances; // Track who owes whom
+
+
+    public Group(String groupName, String groupType, User groupCreator) {
         this.groupName = groupName;
+        this.groupType = groupType;
+        this.groupCreator = groupCreator;
+
         this.members = new ArrayList<>();
-        this.expenses = new ArrayList<Expense>();
-        this.balances = new HashMap<>();
-
-
+        this.originalDebts = new ArrayList<>();
+        this.simplifiedDebts = new ArrayList<>();
     }
 
-    public String getGroupId() {
-        return groupId;
-    }
+    //getters for the above
 
     public String getGroupName() {
         return groupName;
     }
-
-    public List<Expense> getExpenses(){
-        return this.expenses;
+    public String getGroupType() {
+        return groupType;
+    }
+    public User getGroupCreator() {
+        return groupCreator;
     }
 
-
+    public int getGroupId() {
+        return groupId;
+    }
+    public List<User> getMembers() {
+        return members;
+    }
+    public List<Expense> getOriginalDebts() {
+        return this.originalDebts;
+    }
+    public List<Expense> getSimplifiedDebts() {
+        return simplifiedDebts;
+    }
 
 }
--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,26 +1,37 @@
 package entities;
 
 public class User {
-    private String id;
-    private String name;
-    private String password;
+    private final String id;
+    private final String password;
+    private final String email;
+    private final String first_name;
+    private final String last_name;
 
-    public User(String id, String name, String password) {
+    public User(String id, String first_name, String last_name, String email, String password) {
         this.id = id;
-        this.name = name;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
         this.password = password;
     }
 
-    // Getters and setters
+    // Getters
     public String getId() { return id; }
-    public String getName() { return name; }
+    public String getEmail() {
+        return email;
+    }
+    public String getFirst_name() {
+        return first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
     public String getPassword() { return password; }
-
-    public void setName(String name) { this.name = name; }
-    public void setPassword(String email) { this.password = email; }
 
     @Override
     public String toString() {
-        return name;
+        return first_name + " " + last_name;
     }
+
+
 }
