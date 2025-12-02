abee6b63512caa27a8a133adba15a4601f383e8c
Lucy-1123 <lucyluxi.wang@mail.utoronto.ca>
2025-12-01 19:19:47 -0500

    removed to settleup main

--- src/main/view/SettleUpPanel.java
+++ src/main/view/SettleUpPanel.java
@@ -72,11 +72,4 @@ public class SettleUpPanel extends JFrame {
                 area.setText(presenter.getMessage());
                 paidButton.setVisible(false);});
     }
-
-    public static void main(String[] args) {
-
-        Long groupId = 90692920L;
-
-        SwingUtilities.invokeLater(() -> new SettleUpPanel(groupId).setVisible(true));
-    }
 }
